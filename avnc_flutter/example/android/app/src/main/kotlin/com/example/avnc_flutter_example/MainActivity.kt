a20256ccf82b1e09f126a2d86cc38b1dda88ebbd
xodiosx <mohammedaleiwi@gmail.com>
2025-12-04 17:52:18 +0300

    update 1.3

--- avnc_flutter/example/android/app/src/main/kotlin/com/example/avnc_flutter_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.avnc_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- x11_flutter/example/android/app/src/main/kotlin/com/example/x11_flutter_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.x11_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
